d6d2e288a2dda181bb4fff571efcb3368b9baa6b
Dennis Dreesen <dennis.dreesen@skyline.be>
2025-05-06 15:26:07 +0200

    Fix package: Upload Commtia DAB protocol

---
+++ Empower 2025 - AI - Commtia DAB/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empower_2025___AI___Commtia_DAB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
